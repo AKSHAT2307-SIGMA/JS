48e3985cea21f8aec02a99ca12cec8c413e0c773
AKSHAT2307-SIGMA <saxena.akshat.2307@gmail.com>
2025-07-13 19:11:35 +0000

    its datatypess

---
+++ basics/DataTypes.js
@@ -0,0 +1,23 @@
+"use strict" // treat all JS code as newer version
+
+// alert(3 + 3) // we are using Node js not Browser
+
+console.log(3+3)
+
+console.log("Akshat")
+
+let name = "Akshatt"
+let age = 21
+let isLoggedin = false
+let state = null
+
+// number => 2 toPower 53
+// bigint
+// string => true/false
+// null => standalone null
+//undefined
+// symbol => unique
+
+console.log(typeof name)
+console.log(typeof undefined)
+console.log(typeof null)
